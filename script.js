398251bb3361e720621710808788d3f16c553cb3
Zach1281 <zachary_romero@yahoo.com>
2023-08-29 17:13:01 -0700

    Add book and library classes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const display = document.querySelector('.library');
-let myLibrary = [];
 
 class Book{
     constructor(title, author, length, read)  {
@@ -10,23 +9,36 @@ class Book{
     }
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+
+class Library {
+    constructor(){
+        this.myLibrary = [];
+    }
+
+    addBookToLibrary = (book = "unknown") => {
+        this.myLibrary.push(book);
+    }
+
+    removeBookFromLibrary = (book = "unknown") => {
+        for(let i = 0; i < this.myLibrary.length; i++){
+            if(this.myLibrary[i].title === book.title){
+                this.myLibrary[i] = null;
+            }
+        }
+    }
 }
 
-
-
-
+let library = new Library();
 let theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '267', 'read');
 let dune = new Book('Dune', 'Frank Herbert', '617', 'not read');
 
-addBookToLibrary(theHobbit);
-addBookToLibrary(dune);
+library.addBookToLibrary(theHobbit);
+library.addBookToLibrary(dune);
 
-console.log(myLibrary);
+console.log(library);
 
 function displayLibrary(){
-    for(let i = 0; i < myLibrary.length; i++){
+    for(let i = 0; i < library.myLibrary.length; i++){
         const book = document.createElement('div');
         const bookInfo = document.createElement('div');
         const title = document.createElement('div');
@@ -34,13 +46,13 @@ function displayLibrary(){
         const length = document.createElement('div');
         const hasRead = document.createElement('div');
         title.classList.add('title');
-        title.textContent = myLibrary[i].title;
+        title.textContent = library.myLibrary[i].title;
         author.classList.add('author');
-        author.textContent = myLibrary[i].author;
+        author.textContent = library.myLibrary[i].author;
         length.classList.add('length');
-        length.textContent = myLibrary[i].length;
+        length.textContent = library.myLibrary[i].length;
         hasRead.classList.add('has-read');
-        hasRead.textContent = myLibrary[i].read;
+        hasRead.textContent = library.myLibrary[i].read;
         book.classList.add('book' + '-' + i);
         bookInfo.appendChild(title);
         bookInfo.appendChild(author);
@@ -53,3 +65,7 @@ function displayLibrary(){
 }
 
 displayLibrary();
+
+library.removeBookFromLibrary(dune);
+
+
